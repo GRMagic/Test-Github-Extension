e8daebfd32789ed7462789cb4a3c40da9333a58d
GRMagic <gustavo.rm90@gmail.com>
2020-01-14 16:43:41 -0300

    Add project files.

---
+++ Test Github Extension/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Github_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
